a3cf643d35bc16380e177a3386b4f7d1636e6bfe
lehannK <leandro_hollies@hotmail.com>
2023-10-14 17:38:02 -0300

    commit

--- src/Components/PasswordGenerator/index.jsx
+++ src/Components/PasswordGenerator/index.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import "./styles.module.css";
 
 export default () => {
   const [textVar, setTextVar] = useState("Copiar");
   const [currentPassword, setCurrentPassword] = useState("");
+  const [passwordSize, setPasswordSize] = useState(12);
 
   const changeText = (ev) => {
     if (ev.target.id === "btnPassword") {
@@ -19,7 +21,7 @@ export default () => {
     const char =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=[]{}|;:,.<>?";
     let password = "";
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const randomIndex = Math.floor(Math.random() * char.length);
       password += char.charAt(randomIndex);
     }
@@ -28,17 +30,23 @@ export default () => {
 
   return (
     <>
-      <div>
-        <button id="btnPassword" onClick={changeText}>
-          Gerar Senha
-        </button>
-        <p>{currentPassword}</p>
-      </div>
-      <div>
-        <button id="btnCopy" onClick={changeText}>
-          {textVar}
-        </button>
-      </div>
+      <button id="btnPassword" onClick={changeText}>
+        Gerar Senha
+      </button>
+      <p>{currentPassword}</p>
+      <label htmlFor="passwordSize">Tamanho:</label>
+      <input
+        type="number"
+        id="passwordSize"
+        min={1}
+        value={passwordSize}
+        onChange={(ev) => {
+          setPasswordSize(ev.target.value);
+        }}
+      ></input>
+      <button id="btnCopy" onClick={changeText}>
+        {textVar}
+      </button>
     </>
   );
 };
